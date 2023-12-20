40dcb65dbbccb1441547a542344374bcb7d2002a
JihadMuna <jojo.muna.2019@gmail.com>
2023-12-20 23:28:18 +0200

    Finishing Site

--- src/Pages/CustomProduct.jsx
+++ src/Pages/CustomProduct.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import styles from '../Pages/styles/CustomProduct.module.css'
+import React from "react";
+import styles from "../Pages/styles/CustomProduct.module.css";
 
 function CustomProduct() {
   return (
     <div className={styles.customCont}>
-      <h1 className={styles.custom}>Comming Soon ...</h1>
+      <h1 className={styles.custom}>Coming Soon ...</h1>
     </div>
-  )
+  );
 }
 
-export default CustomProduct
+export default CustomProduct;
